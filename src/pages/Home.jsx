c25e47b2e84e244c9a4004497796a08761b3bba6
guptaji <akhilshorts87@gmail.com>
2025-02-22 11:03:06 +0530

    MissionSection will added to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Heading from '../components/Header/Heading'
 import Nav from '../components/Header/Nav'
 import HeroSection from '../components/Section/HeroSection'
+import MissionSection from '../components/Section/MissionSection'
 
 function Home() {
   return (
@@ -11,6 +12,7 @@ function Home() {
       <div className="bg-gradient-to-t from-[#030711] via-[#050D1E] via-50% to-[#0A1A3C] to-100% min-h-screen">
       <Nav/>
         <HeroSection />
+        <MissionSection/>
       </div>
     </>
   )
